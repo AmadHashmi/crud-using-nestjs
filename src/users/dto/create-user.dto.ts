7a90cf1fcbdb51d17b8cfa473b22ac5faf97f7f7
Amad Ul Hassan <ah@indigilabs.com>
2025-06-29 13:14:32 +0200

    added user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,40 @@
+import {
+  ArrayContains,
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { PREDEFINED_GROUPS, PREDEFINED_ROLES } from '../user.model';
+
+export class UserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  name?: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayContains(PREDEFINED_ROLES, { each: true })
+  roles?: string[];
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayContains(PREDEFINED_GROUPS, { each: true })
+  groups?: string[];
+}
+
+export class UpdateUserDto {
+  @IsString()
+  @MaxLength(100)
+  name?: string;
+
+  @IsString()
+  @ArrayContains(PREDEFINED_ROLES, { each: true })
+  roles?: string[];
+
+  @IsArray()
+  @ArrayContains(PREDEFINED_GROUPS, { each: true })
+  groups?: string[];
+}
